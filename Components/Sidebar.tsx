729094f095c1d47aa13365d053084de45f67b2f1
hiren <72260974+hirensavani-99@users.noreply.github.com>
2022-08-08 09:42:51 +0200

    sidebarrow changed v1

--- Components/Sidebar.tsx
+++ Components/Sidebar.tsx
@@ -10,6 +10,7 @@ import {
 } from "@heroicons/react/outline"
 import { signIn, signOut, useSession } from "next-auth/react"
 import SidebarRow from "./SidebarRow"
+import SidebarRow1 from "./SidebarRow1"
 
 function Sidebar() {
 
@@ -20,14 +21,14 @@ function Sidebar() {
         <div className="flex flex-col col-span-2 items-center px-4 md:items-start">
             <img className="h-10 w-10" src="https://links.papareact.com/drq" alt="" />
 
-            <SidebarRow Icon={HomeIcon} title="Home" />
-            <SidebarRow Icon={HashtagIcon} title="Explore" />
-            <SidebarRow Icon={BellIcon} title="Notification" />
-            <SidebarRow Icon={MailIcon} title="Messages" />
-            <SidebarRow Icon={BookmarkIcon} title="BookMark" />
-            <SidebarRow Icon={CollectionIcon} title="Lists" />
+            <SidebarRow1 Icon={HomeIcon} title="Home" />
+            <SidebarRow1 Icon={HashtagIcon} title="Explore" />
+            <SidebarRow1 Icon={BellIcon} title="Notification" />
+            <SidebarRow1 Icon={MailIcon} title="Messages" />
+            <SidebarRow1 Icon={BookmarkIcon} title="BookMark" />
+            <SidebarRow1 Icon={CollectionIcon} title="Lists" />
             <SidebarRow onClick={session ? signOut : signIn} Icon={UserIcon} title={session ? "Sign out" : "Sign in"} />
-            <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
+            <SidebarRow1 Icon={DotsCircleHorizontalIcon} title="More" />
         </div>
     )
 }
